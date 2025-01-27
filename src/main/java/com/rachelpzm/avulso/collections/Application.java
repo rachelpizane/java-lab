a5794c13f0937aeb982c3791c718838192cf2c18
Rachel Pizane <rachel.pzm@gmail.com>
2025-01-27 11:32:26 -0300

    comecei a estudar sobre collections

---
+++ src/main/java/com/rachelpzm/avulso/collections/Application.java
@@ -0,0 +1,18 @@
+package com.rachelpzm.avulso.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.print("Oi");
+        Collection colecao;
+        List lista1 = new ArrayList<>();
+        ArrayList lista;
+        LinkedList lista2;
+        Comparator c;
+    }
+}
