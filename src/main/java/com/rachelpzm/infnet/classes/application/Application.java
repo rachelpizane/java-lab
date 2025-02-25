167b4d2e6e32f918462a06eeef36afad83a42774
Rachel Pizane <rachel.pzm@gmail.com>
2025-02-24 22:06:34 -0300

    aprendendo sobre POO

---
+++ src/main/java/com/rachelpzm/infnet/classes/application/Application.java
@@ -0,0 +1,56 @@
+package com.rachelpzm.infnet.classes.application;
+
+import com.rachelpzm.infnet.classes.domain.ContaCorrente;
+
+import java.util.Scanner;
+
+public class Application {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        ContaCorrente cc = new ContaCorrente(1234, "Carla Madeira", 1234.56);
+        String opcao = "";
+
+        while (!opcao.equals("9")) {
+            System.out.println("\n-----------------------------");
+            System.out.println("BANCO MONEY -- CONTA-CORRENTE");
+            System.out.println("-----------------------------");
+            System.out.println("1 - Depositar");
+            System.out.println("2 - Sacar");
+            System.out.println("9 - Sair");
+
+            System.out.print("Selecione a opção: ");
+            opcao = input.nextLine();
+
+            double valor;
+            switch (opcao) {
+                case "1":
+                    valor = solicitarValor("depósito");
+                    cc.depositar(valor);
+                    break;
+
+                case "2":
+                    valor = solicitarValor("saque");
+                    cc.sacar(valor);
+                    break;
+
+                case "9":
+                    break;
+                default:
+                    System.out.println("[ERRO] Entrada inválida.");
+                    continue;
+            }
+
+            System.out.printf(cc.obterSaldo());
+        }
+    }
+
+    private static double solicitarValor(String complemento){
+        System.out.printf("Digite o valor do %s: ", complemento);
+        String sValor = input.nextLine();
+
+        return Double.parseDouble(sValor);
+    }
+
+
+}
---
+++ src/main/java/com/rachelpzm/infnet/classes/domain/ContaCorrente.java
@@ -0,0 +1,29 @@
+package com.rachelpzm.infnet.classes.domain;
+
+public class ContaCorrente {
+    private int numero;
+    private String titular;
+    private double saldo;
+
+    public ContaCorrente(int numero, String titular, double saldo){
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public void depositar(double valor) {
+        if(valor > 0){
+            saldo += valor;
+        }
+    }
+
+    public void sacar(double valor){
+        if( valor <= saldo){
+            saldo -= valor;
+        }
+    }
+
+    public String obterSaldo() {
+        return numero + " - " + titular + " - " + saldo;
+    }
+}
